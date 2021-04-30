1bcfd515b7350ec76db5c24b8e24dc24db6c404c
tanzeel <tanzeelwahid111@gmail.com>
2021-05-01 03:03:45 +0500

    chat app react chat engine

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
 function App() {
+
+  if (!localStorage.getItem("username")) return <LoginForm />
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+    height="100vh"
+    projectID="b2db8cc3-452a-4ce1-b785-4e374d9146fe"
+    userName= {localStorage.getItem("username")}
+    userSecret={localStorage.getItem("password")}
+
+    renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+    />
   );
 }
 
